ba947c82aee054eff9be12c8bf8bc7689be36ff7
Chris Ramer <chrisramer.97@gmail.com>
2020-08-19 13:57:41 -0700

    Add question 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 	
+	// Start quiz
 	$("#button").click(function () { 
 		$(".preQuiz").hide();
 		$("#quiz").show();
 	});
 
+	// Submit question 1
 	$("form#question1").submit(function (e) { 
 		e.preventDefault();
 		
@@ -12,6 +14,7 @@ $(document).ready(function () {
 		$("#question2").show();
 	});
 
+	// Submit question 2
 	$("form#question2").submit(function (e) {
 		e.preventDefault();
 
@@ -19,11 +22,27 @@ $(document).ready(function () {
 		$("#question3").show();
 	});
 
+	// Submit question 3
 	$("form#question3").submit(function (e) {
 		e.preventDefault();
 
 		$("#question3").hide();
 		$("#question4").show();
+	});
+
+	// Submit question 4
+	$("form#question4").submit(function (e) {
+		e.preventDefault();
+
+		$("#question4").hide();
+		$("#question5").show();
+	});
+	
+	// Submit last question
+	$("form#question5").submit(function (e) { 
+		e.preventDefault();
+		
+		
 	});
 
 });
